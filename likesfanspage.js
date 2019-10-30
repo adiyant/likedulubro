6e05fac02f7d39b4aef75687d89c8f6aadb2b04a
adiyant <50225525+adiyant@users.noreply.github.com>
2019-10-30 11:40:50 +0700

    Add files via upload

---
+++ likesfanspage.js
@@ -0,0 +1,23 @@
+<script src='http://code.jquery.com/jquery-1.10.2.min.js' type='text/javascript'/>
+<link href='https://www.dropbox.com/s/j8u0px1qszixxwl/TricksTantraSCL.css?dl=1' rel='stylesheet' type='text/css'/>
+<script src='https://www.dropbox.com/s/tskmt978ku8qb7g/TricksTantraSCL.js?dl=1' type='text/javascript'/>
+<script type='text/javascript'>
+//<![CDATA[
+$(function(){
+  $('.socialLocker .to-lock').sociallocker({
+    buttons: {order:["facebook-like","twitter-tweet","instagram-ig"]},
+    twitter: {url:"https://twitter.com/amanahbilal1"},
+    facebook: {url:"https://www.facebook.com/InspirasiMilenial/?modal=admin_todo_tour"},
+    google: {url:"https://www.instagram.com/pusatbisnis_ecoracing/?hl=id"},
+    text: {
+      header: "Like us To Unlock This Content",
+      message: "This content is locked. Like us on Twitter, Facebook or Google plus to unlock it."
+    },
+    locker: {close: false, timer: 0,},
+    theme: "secrets"
+  });
+});
+//]]>
+</script>
+
+                                       
